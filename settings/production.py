46c93a2f8cfdcb6f73b133f1c912d3ae6d7c286a
Sergey <serg78kh@yandex.ru>
2020-12-09 21:33:26 +0300

    SmartNotesBot v1.0 debug demon config v1.5

--- settings/production.py
+++ settings/production.py
@@ -1,5 +1,6 @@
 # TG_TOKEN = "1363841624:AAEbh2XaTpf0wVK6nJaDTY6mxNs4gtpjTBY"
 # TG_API_URL = None
+import logging.config
 import os
 
 import sentry_sdk
@@ -8,14 +9,12 @@ import sentry_sdk
 # Путь до коренной папки `tele_bot`
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
 
-# вписать сюда токен от своего бота!!!
 TG_TOKEN = "1363841624:AAEbh2XaTpf0wVK6nJaDTY6mxNs4gtpjTBY"
 
 # На сервере не используем прокси-URL
 TG_API_URL = None
 
 # Sentry
-# добавить свой DSN из https://sentry.io
 sentry_sdk.init(
-    dsn="https://4dd83f6842b04b4db2217119886364a6@o489213.ingest.sentry.io/5551048",
+    dsn="1363841624:AAEbh2XaTpf0wVK6nJaDTY6mxNs4gtpjTBY",
 )
